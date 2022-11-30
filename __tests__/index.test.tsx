b4e224e8ff95cd8924ea7f2cd369681a05232826
Jerome Vonk <lelovv@gmail.com>
2022-11-30 15:51:53 -0300

    act?

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,9 +1,11 @@
-import { render, screen } from '@testing-library/react'
+import { act, render, screen } from '@testing-library/react'
 import Home from '@/pages/index'
 
 describe('Index', () => {
   it('renders a footer', () => {
-    render(<Home />)
+    act(() => {
+      render(<Home />)
+    });
 
     const footer = screen.getByRole('link', {
       name: "By Jerome Vonk",
--- __tests__/snapshot.tsx
+++ __tests__/snapshot.tsx
@@ -1,7 +1,11 @@
-import { render } from '@testing-library/react'
+import { act, render } from '@testing-library/react'
 import Home from '../pages/index'
 
 it('renders homepage unchanged', () => {
-  const { container } = render(<Home />)
-  expect(container).toMatchSnapshot()
+  
+  act(() => {
+    const { container } = render(<Home />)
+    expect(container).toMatchSnapshot()
+  });
+  
 })
