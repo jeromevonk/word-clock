aff9a6d9ece418441225b8ae5c69ce183e5c5d51
Jerome Vonk <lelovv@gmail.com>
2024-08-27 18:29:38 -0300

    add tests util

--- __tests__/util.test.tsx
+++ __tests__/util.test.tsx
@@ -1,8 +1,10 @@
-import { 
-  convertTimeToWords, 
-  convertTimeToDigits, 
-  isEquivalent 
+import { act } from 'react-dom/test-utils';
+import {
+  convertTimeToWords,
+  convertTimeToDigits,
+  isEquivalent
 } from '@/helpers/util'
+import { NumberMatrix } from "@/types/types"
 
 const expected_4_14_words = [
   [1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0],
@@ -40,6 +42,45 @@ const expected_12_45_words = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
+
+const expected_6_20_words_PT = [
+  [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0],
+  [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
+
+const expected_6_20_words_FR = [
+  [1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
+
+const expected_6_25_words_ES = [
+  [0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+  [0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ];
 
@@ -84,36 +125,78 @@ const expected_12_45_digits = [
 
 describe('Util', () => {
   it('converts time to words (EN) - 4:14', () => {
-    const state = convertTimeToWords("4:14 pm", 'EN');
+    let state: NumberMatrix = [];
+    act(() => {
+      state = convertTimeToWords("4:14 pm", 'EN');
+    });
     expect(isEquivalent(state, expected_4_14_words)).toBe(true);
   });
 
   it('converts time to words (EN) - 11:59', () => {
-    const state = convertTimeToWords("11:59 pm", 'EN');
+    let state: NumberMatrix = [];
+    act(() => {
+      state = convertTimeToWords("11:59 pm", 'EN');
+    });
     expect(isEquivalent(state, expected_11_59_words)).toBe(true);
   });
 
   it('converts time to words (EN) - 12:45', () => {
-    const state = convertTimeToWords("12:45 pm", 'EN');
+    let state: NumberMatrix = [];
+    act(() => {
+      state = convertTimeToWords("12:45 pm", 'EN');
+    });
     expect(isEquivalent(state, expected_12_45_words)).toBe(true);
   });
 
-  it('converts time to digits - 12:45', () => {
-    const state = convertTimeToDigits("4:14 pm");
+  it('converts time to words (PT) - 6:20', () => {
+    let state: NumberMatrix = [];
+    act(() => {
+      state = convertTimeToWords("6:20 pm", 'PT');
+    });
+    expect(isEquivalent(state, expected_6_20_words_PT)).toBe(true);
+  });
+
+  it('converts time to words (FR) - 6:20', () => {
+    let state: NumberMatrix = [];
+    act(() => {
+      state = convertTimeToWords("6:20 pm", 'FR');
+    });
+    expect(isEquivalent(state, expected_6_20_words_FR)).toBe(true);
+  });
+
+  it('converts time to words (ES) - 6:25', () => {
+    let state: NumberMatrix = [];
+    act(() => {
+      state = convertTimeToWords("6:25 pm", 'ES');
+    });
+    expect(isEquivalent(state, expected_6_25_words_ES)).toBe(true);
+  });
+
+  it('converts time to digits - 4:14', () => {
+    let state: NumberMatrix = [];
+    act(() => {
+      state = convertTimeToDigits("4:14 pm");
+    });
     expect(isEquivalent(state, expected_4_14_digits)).toBe(true);
   });
 
-  it('converts time to digits - 12:45', () => {
-    const state = convertTimeToDigits("11:59 pm");
+  it('converts time to digits - 11:59', () => {
+    let state: NumberMatrix = [];
+    act(() => {
+      state = convertTimeToDigits("11:59 pm");
+    });
     expect(isEquivalent(state, expected_11_59_digits)).toBe(true);
   });
 
   it('converts time to digits - 12:45', () => {
-    const state = convertTimeToDigits("12:45 pm");
+    let state: NumberMatrix = [];
+    act(() => {
+      state = convertTimeToDigits("12:45 pm");
+    });
     expect(isEquivalent(state, expected_12_45_digits)).toBe(true);
   });
 
   it('isEquivalent false', () => {
     expect(isEquivalent(expected_11_59_digits, expected_12_45_digits)).toBe(false);
   });
-})
+});
